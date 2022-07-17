33db4c6da566128021037bdbe38cb260afe5d0a6
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-17 16:15:47 +0530

    added city autocomplete api params

---
+++ src/api.js
@@ -0,0 +1,11 @@
+const GEO_API_OPTIONS = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "3f49394ba6msha59566260dff6ebp1988e1jsn90d496a6efdc",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+const GEO_API_URL = `https://wft-geo-db.p.rapidapi.com/v1/geo`;
+
+export { GEO_API_OPTIONS, GEO_API_URL };
